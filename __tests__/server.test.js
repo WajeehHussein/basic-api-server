5fa16794b5295330dac7fd15f0b6543315d81b67
WajeehHussein <wajehessam3@gmail.com>
2022-06-06 16:56:02 +0300

    end

---
+++ __tests__/server.test.js
@@ -0,0 +1,51 @@
+'use strict';
+const { app } = require('../src/server')
+
+const supertest = require('supertest')
+const mockRequest = supertest(app);
+
+const { db } = require('../src/models/index')
+
+beforeAll(async () => {
+    await db.sync();
+})
+
+describe('', () => {
+    test('not Found 404 bad route', async () => {
+        const res = await mockRequest.get('/not');
+        expect(res.status).toBe(404);
+    })
+    test('not Found 404 bad method', async () => {
+        const res = await mockRequest.del('/not');
+        expect(res.status).toBe(404);
+    })
+    test('add a food', async () => {
+        const res = await mockRequest.post('/food').send(
+            {
+                name: "mansaf",
+                price: "22"
+            }
+        );
+        expect(res.status).toBe(201);
+    })
+    test('get all data', async () => {
+        const res = await mockRequest.get('/food');
+        expect(res.status).toBe(200)
+    })
+    test('get a food ( one )', async () => {
+        const res = await mockRequest.get('/food/1');
+        expect(res.status).toBe(200)
+    })
+    test('update a food ( one )', async () => {
+        const res = await mockRequest.put('/food/1');
+        expect(res.status).toBe(201)
+    })
+    test('delete a food ( one )', async () => {
+        const res = await mockRequest.put('/food/1');
+        expect(res.status).toBe(204)
+    })
+})
+
+afterAll(async () => {
+    await db.drop();
+})
